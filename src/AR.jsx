728b88c870e39805aef8439dfc96241efa28ebcd
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2025-01-28 20:02:12 +0530

    add

--- src/AR.jsx
+++ src/AR.jsx
@@ -7,7 +7,7 @@ const AR = () => {
         <a-marker preset="hiro">
           <a-entity
             position="0 0 0"
-            scale="2 2 2"
+            scale="1 1 1"
             gltf-model="url(/models/product_model.glb)"
             rotation="0 0 0"
           ></a-entity>
