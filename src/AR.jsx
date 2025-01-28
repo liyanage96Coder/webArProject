f4f8db10fd9ffed570884649f60e619296fb9fe4
Mithila Sriyanjana <mithila.sriyanjana@gmail.com>
2025-01-28 19:23:50 +0530

    first commit

---
+++ src/AR.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AR = () => {
+  return (
+    <div id="ar-container">
+      <a-scene embedded arjs>
+        <a-marker preset="hiro">
+          <a-entity
+            position="0 0 0"
+            scale="2 2 2"
+            gltf-model="url(/models/product_model.glb)"
+            rotation="0 0 0"
+          ></a-entity>
+        </a-marker>
+        <a-camera></a-camera>
+      </a-scene>
+    </div>
+  );
+};
+
+export default AR;
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import AR from './AR';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WebAR Product Promotion</h1>
+      <AR />
+    </div>
+  );
+}
+
+export default App;
